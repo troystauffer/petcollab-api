b1cfbca2b0c34aa14ab59431e486aca3691dda6d
Troy Stauffer <troystauffer@gmail.com>
2017-04-13 14:23:18 -0500

    updates migration for postgres compatibility

--- migrations/20170411041238-update-user-add-role.js
+++ migrations/20170411041238-update-user-add-role.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.addColumn(
-      'Users',
+      'users',
       'role_id',
       {
         type: Sequelize.INTEGER
@@ -13,7 +13,7 @@ module.exports = {
 
   down: function (queryInterface, Sequelize) {
     return queryInterface.removeColumn(
-      'Users',
+      'users',
       'rold_id'
     );
   }
